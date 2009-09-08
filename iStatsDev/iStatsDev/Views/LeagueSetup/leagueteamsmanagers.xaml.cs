ca7bfdb6b6d2f8203ad21db31caa6aaac105ad10
schmide1 <schmide1@yahoo.com>
2009-09-08 15:13:24 -0400

    Added League Creation template

---
+++ iStatsDev/iStatsDev/Views/LeagueSetup/leagueteamsmanagers.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace iStatsDev
+{
+    public partial class leagueteamsmanagers : Page
+    {
+        public leagueteamsmanagers()
+        {
+            InitializeComponent();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void HyperlinkButton_BindingValidationError(object sender, System.Windows.Controls.ValidationErrorEventArgs e)
+        {
+        	// TODO: Add event handler implementation here.
+        }
+    }
+}
