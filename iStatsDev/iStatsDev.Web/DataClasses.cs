24bb33e52c10cc40e934bda77b8ea8a4dd1af518
bbaros <boki@yahoo.com>
2009-09-09 19:13:18 -0400

    Added database classes and Web Service

---
+++ iStatsDev/iStatsDev.Web/DataClasses.cs
@@ -0,0 +1,55 @@
+namespace iStatsDev.Web
+{
+    partial class DataClassesDataContext
+    {
+    }
+    partial class User
+    {
+        public enum userTypes : int
+        {
+            Admin = 0,
+            Mod = 1,
+            LeagueAdmin = 2,
+            TeamCoach = 3,
+            Player = 4,
+            ScoreRecorder = 5,
+            Fan = 6
+        }
+
+        public enum userStatus : int    
+        {
+            Pending = 0,
+            Active = 1,
+            Locked = 2,
+            Closed = 3
+        }
+
+        public enum userGender : int
+        {
+            Male = 0,
+            Female = 1
+        }
+
+        public Address address;
+    }
+
+    partial class Event
+    {
+        public enum EventType : int
+        {
+            Information = 0,
+            Warning = 1,
+            Error = 2
+        }
+
+        public static void logEvent(EventType evntType, int userId, string message)
+        {
+            Event evnt = new Event();
+
+            evnt.DateTime = System.DateTime.Now.ToUniversalTime();
+            evnt.Message = message;
+            evnt.Type = (int)evntType;
+            evnt.UserID = userId;
+        }
+    }
+}
